87ce98f5b7ff0f65f5b4bb53a8c41ad6ab983062
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-28 11:58:33 +0600

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,51 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import type { GitUpdateRefResponseData, OctokitResponse } from '@octokit/types';
+
+async function run(): Promise<void> {
+  try {
+    const githubToken = core.getInput('github-token');
+    const sourceBranchName = core.getInput('source');
+    const destBranchName = core.getInput('dest');
+
+    const octokit = github.getOctokit(githubToken);
+    const sourceBranchTip = await octokit.git.getRef({
+      ...github.context.repo,
+      ref: `heads/${sourceBranchName}`,
+    });
+
+    if (sourceBranchTip.data.object.type !== "commit") {
+      throw new Error(`Expected branch ${sourceBranchName} to resolve to a commit. Got a ${sourceBranchTip.data.object.type}.`);
+    }
+
+    const sourceBranchSha = sourceBranchTip.data.object.sha;
+
+    console.log(`Pushing ${sourceBranchName} (${sourceBranchSha}) to ${destBranchName}.`);
+
+    let result: OctokitResponse<GitUpdateRefResponseData>;
+    try {
+      result = await octokit.git.updateRef({
+        ...github.context.repo,
+        ref: `heads/${destBranchName}`,
+        sha: sourceBranchSha,
+        force: true,
+      });
+    } catch (error: any) {
+      if (error.message !== 'Reference does not exist') {
+        throw error;
+      }
+      console.log(`${destBranchName} does not exist. Creating it.`);
+      result = await octokit.git.createRef({
+        ...github.context.repo,
+        ref: `refs/heads/${destBranchName}`,
+        sha: sourceBranchSha,
+      });
+    }
+
+    console.log(`Set ${result.data.ref} to ${result.data.object.sha}.`);
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
